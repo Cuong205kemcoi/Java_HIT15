965c5db9f83eff42c1db8001df84a622233abfd1
Cuong <lc02022005@gmail.com>
2024-11-05 21:49:12 +0700

    Thay đổi

---
+++ Buổi1/test.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class test{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("\nNhập vào một chuỗi kí tự :");
+        String input =sc.nextLine();
+        System.out.println("\nChuỗi sau khi nhập là :"+input);
+    }
+}
